16a08eaf60bbafe7c83e02c02ec29393fb2fb34c
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-18 16:12:01 -0500

    Problema propuesto: imprimir varias cadenas

---
+++ S03-Programa-PHP/P0301-impresiones.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>P0301-impresiones</title>
+  </head>
+  <body>
+    <?php
+      echo "PHP es ";
+      echo "un lenguaje de programación.";
+      echo "<br>";
+
+      echo "Estamos aprendiendo los esenciales de este lenguaje.";
+    ?>
+  </body>
+</html>
