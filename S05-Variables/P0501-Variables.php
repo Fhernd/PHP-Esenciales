931b381d6fe188e53579f4c731189618c4e689ec
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-04-20 15:23:08 -0500

    Definición e impresión de variables

---
+++ S05-Variables/P0501-Variables.php
@@ -0,0 +1,13 @@
+<?php
+
+// Definir una variable de cada tipo: integer, double, string y boolean. Luego imprimirlas en la página, una por línea.
+
+$puntos = 1500;
+$nombre = 'infZer0';
+$calificacion = 75.9;
+$tieneCuentaVip = true;
+
+echo 'Cantidad de puntos: ' . $puntos . '<br>';
+echo 'Nombre: ' . $nombre . '<br>';
+echo 'Calificación: ' . $calificacion . '<br>';
+echo '¿Es VIP?: ' . $tieneCuentaVip . '<br>';
