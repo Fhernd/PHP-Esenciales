b8587657b709cef6009f9e53028179033898b92c
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-05-02 16:51:44 -0500

    Formulario para capturar la edad de una persona

---
+++ S09-Formularios/P0901-mayor-edad.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+  <title>Formulario de entrada del dato</title>
+</head>
+
+<body>
+
+  <form method="post" action="P0901-procesamiento.php">
+    Ingrese su edad:
+    <input type="number" name="edad">
+    <br>
+    <input type="submit" value="confirmar">
+  </form>
+
+</body>
+
+</html>
