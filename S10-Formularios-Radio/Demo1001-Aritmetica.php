8547bfd41f924a7510a7abe27594e790314836eb
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-05-03 16:39:23 -0500

    Suma o resta de dos números

---
+++ S10-Formularios-Radio/Demo1001-Aritmetica.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+
+  <form action="Demo1001-Aritmetica-Procesamiento.php" method="post">
+    Ingrese primer número:
+    <input type="number" name="numero1">
+    <br>
+    Ingrese segundo número:
+    <input type="number" name="numero2">
+    <br>
+    <input type="radio" name="operacion" value="sumar">Sumar
+    <br>
+    <input type="radio" name="operacion" value="restar">Restar
+    <br>
+    <input type="submit" name="Operar">
+  </form>
+
+</body>
+
+</html>
