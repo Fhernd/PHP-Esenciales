bdde0bc586ea122785e843b672b4b09a24f898ef
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-05-06 16:08:56 -0500

    Operaciones para el select en el servidor

---
+++ S12-Formularios-Select/Demo1201-Formularios-select-operar.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_REQUEST['operacion'] == 'sumar') {
+    $numero1 = floatval($_REQUEST['numero1']);
+    $numero2 = floatval($_REQUEST['numero2']);
+
+    $suma = $numero1 + $numero2;
+
+    echo "La suma de $numero1 más $numero2 es igual a $suma.";
+} elseif ($_REQUEST['operacion'] == 'restar') {
+    $numero1 = floatval($_REQUEST['numero1']);
+    $numero2 = floatval($_REQUEST['numero2']);
+
+    $resta = $numero1 + $numero2;
+
+    echo "La resta de $numero1 menos $numero2 es igual a $resta.";
+}
