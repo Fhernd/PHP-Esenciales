854b3da6015f0aa2e9989036b9f61c868650b7fb
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-06-11 12:31:58 -0500

    Uso básico de los vectores asociativos

--- S17-Vectores-Asociativos/Demo1701-Intro-Vectores-Asociativos.php
+++ S17-Vectores-Asociativos/Demo1701-Intro-Vectores-Asociativos.php
@@ -17,3 +17,22 @@ for($i=0;$i<count($registro);$i++){ // 0, 1, 2
     echo $registro[$i] . "<br>";
 }
 
+echo "<br>";
+echo "<br>";
+
+echo "Uso de vectores basados en índices asociativos<br>";
+
+$registro2["dni"] = "20456322";
+$registro2["nombre"] = "Martinez Pablo";
+$registro2["direccion"] = "Colon 134";
+
+echo $registro2["dni"] . "<br>";
+echo $registro2["direccion"] . "<br>";
+echo $registro2["nombre"] . "<br>";
+
+echo "<br>";
+echo "<br>";
+
+foreach($registro2 as $clave => $valor){
+    echo $clave . ": " . $valor . "<br>";
+}
