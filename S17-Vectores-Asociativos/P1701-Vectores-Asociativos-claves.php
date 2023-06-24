9848905ff7ee36f12225a2c9cbb7bd9ef19aa75c
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-06-24 17:40:16 -0500

    Problema de claves de usuarios

---
+++ S17-Vectores-Asociativos/P1701-Vectores-Asociativos-claves.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Enunciado:
+ * 
+ * Crear un vector asociativo que almacene las claves de acceso de 5 usuarios de un sistema. 
+ * Acceder a cada componente por su nombre. Imprimir una componente del vector.
+ */
+
+$usuarios['usuario1'] = '@v10n';
+$usuarios['usuario2'] = '@-123***456-@';
+$usuarios['usuario3'] = 'j73Kxv9z';
+$usuarios['usuario4'] = 'sG4tL6dE';
+$usuarios['usuario5'] = 'sG4tL6dE';
+
+echo "El usuario 1 tiene la clave: " . $usuarios['usuario1'] . "<br>";
+echo "El usuario 2 tiene la clave: " . $usuarios['usuario2'] . "<br>";
