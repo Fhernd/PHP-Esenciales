b4c098d3c8ba01cece0f053c241d5166c5d6c01b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-01 11:19:49 -0500

    Pasar parámetros por referencia a una función

--- S18-Funciones/Demo1803-Funciones-cuadrado-cubo.php
+++ S18-Funciones/Demo1803-Funciones-cuadrado-cubo.php
@@ -12,10 +12,16 @@
         $cubo = $valor * $valor * $valor;
     }
 
-    calcularCuadradoCubo(2, $c1, $c2);
+    $cuadrado = 0;
+    $cubo = 0;
+
+    echo "El valor de la variable cuadrado antes de invocar la funcion es: " . $cuadrado . "<br>";
+    echo "El valor de la variable cubo antes de invocar la funcion es: " . $cubo . "<br>";
+
+    calcularCuadradoCubo(2, $cuadrado, $cubo);
     
-    echo "El cuadrado de 2 es:" . $c1 . "<br>";
-    echo "El cubo de 2 es:" . $c2;
+    echo "El cuadrado de 2 es:" . $cuadrado . "<br>";
+    echo "El cubo de 2 es:" . $cubo;
     ?>
 </body>
 
