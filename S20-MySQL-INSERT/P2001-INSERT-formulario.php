b300ff94288d74d1c747637f1318b71781055209
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-14 14:48:14 -0500

    Formulario para dar de alta un curso

---
+++ S20-MySQL-INSERT/P2001-INSERT-formulario.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <h1>Alta de Curso</h1>
+    <form action="P2001-INSERT-almacenamiento-curso.php" method="post">
+        Ingrese nombre:
+        <input type="text" name="nombre"><br>
+        <input type="submit" value="Registrar">
+    </form>
+</body>
+
+</html>
