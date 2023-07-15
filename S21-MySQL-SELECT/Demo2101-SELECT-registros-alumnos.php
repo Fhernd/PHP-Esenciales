4e4bfad492a4d545533adb95919e316b0ee2bb00
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-15 09:57:47 -0500

    Corrección de nombre de campos y tablas

--- S21-MySQL-SELECT/Demo2101-SELECT-registros-alumnos.php
+++ S21-MySQL-SELECT/Demo2101-SELECT-registros-alumnos.php
@@ -7,19 +7,19 @@
 <body>
 
   <?php
-  $conexion = mysqli_connect("localhost", "root", "", "base1") or
+  $conexion = mysqli_connect("localhost", "root", "", "academia") or
     die("Problemas con la conexión");
 
-  $registros = mysqli_query($conexion, "select codigo,nombre, mail, codigocurso
-                        from alumnos") or
+  $registros = mysqli_query($conexion, "SELECT codigo, nombre, correo, codigo_curso
+                        FROM alumno") or
     die("Problemas en el select:" . mysqli_error($conexion));
 
   while ($reg = mysqli_fetch_array($registros)) {
     echo "Codigo:" . $reg['codigo'] . "<br>";
     echo "Nombre:" . $reg['nombre'] . "<br>";
-    echo "Mail:" . $reg['mail'] . "<br>";
+    echo "Mail:" . $reg['correo'] . "<br>";
     echo "Curso:";
-    switch ($reg['codigocurso']) {
+    switch ($reg['codigo_curso']) {
       case 1:
         echo "PHP";
         break;
