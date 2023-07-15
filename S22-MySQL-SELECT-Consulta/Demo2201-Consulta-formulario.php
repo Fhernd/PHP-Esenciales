3df3da1e7f6ac3dda68bda3a3a00819021c1d0ab
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-15 12:18:52 -0500

    Formulario para consultar un estudiante por su correo

---
+++ S22-MySQL-SELECT-Consulta/Demo2201-Consulta-formulario.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <form action="Demo2201-Consulta-base-datos.php" method="post">
+    Ingrese el correo del alumno a consultar:
+    <input type="email" name="correo">
+    <br>
+    <input type="submit" value="Buscar">
+  </form>
+</body>
+
+</html>
