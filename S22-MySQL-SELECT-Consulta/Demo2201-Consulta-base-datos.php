edee33db7d49ac27439cf515fac295e49e3aa68b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-15 12:20:28 -0500

    Consulta en la tabla alumno con una condicional

---
+++ S22-MySQL-SELECT-Consulta/Demo2201-Consulta-base-datos.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <?php
+    $conexion = mysqli_connect("localhost", "root", "", "academia") or
+        die("Problemas con la conexión");
+
+    $registros = mysqli_query($conexion, "SELECT codigo, nombre, codigo_curso
+                        FROM alumnos WHERE correo='$_REQUEST[correo]'") or
+        die("Problemas en el select:" . mysqli_error($conexion));
+
+    if ($reg = mysqli_fetch_array($registros)) {
+        echo "Nombre:" . $reg['nombre'] . "<br>";
+        echo "Curso:";
+        switch ($reg['codigo_curso']) {
+            case 1:
+                echo "PHP";
+                break;
+            case 2:
+                echo "ASP";
+                break;
+            case 3:
+                echo "JSP";
+                break;
+        }
+    } else {
+        echo "No existe un alumno con ese mail.";
+    }
+    mysqli_close($conexion);
+    ?>
+</body>
+
+</html>
