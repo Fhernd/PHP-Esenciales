e0c1205c016e17e76ad6936ea3ff01a8b963b36e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-17 14:44:30 -0500

    Formulario para buscar alumnos a partir de su nombre

---
+++ S22-MySQL-SELECT-Consulta/P2201-Consulta-formulario-alumno.php
@@ -0,0 +1,17 @@
+<!-- Confeccionar un programa que permita ingresar el nombre de un alumno en un formulario, luego mostrar los datos del mismo (tener en cuenta que puede haber más de un alumno con el mismo nombre) -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta</title>
+</head>
+<body>
+    <form action="P2201-Consulta-alumno-nombre.php" method="POST">
+        Ingrese el nombre del alumno a consultar:
+        <input type="text" name="nombre">
+        <br>
+        <input type="submit" value="Buscar">
+    </form>
+</body>
+</html>
