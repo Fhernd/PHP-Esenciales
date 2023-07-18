2c88c5eb67884b713432b69b76d489c9d3bc7c78
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-18 14:49:54 -0500

    Eliminación satisfactoria de un alumno

--- S23-MySQL-DELETE/Demo2301-DELETE-eliminar-alumno.php
+++ S23-MySQL-DELETE/Demo2301-DELETE-eliminar-alumno.php
@@ -9,12 +9,12 @@
     $conexion = mysqli_connect("localhost", "root", "", "academia") or
         die("Problemas con la conexión");
 
-    $registros = mysqli_query($conexion, "SELECT codigo FROM alumnos
-                        WHERE mail='$_REQUEST[correo]'") or
+    $registros = mysqli_query($conexion, "SELECT codigo FROM alumno
+                        WHERE correo='$_REQUEST[correo]'") or
         die("Problemas en el select:" . mysqli_error($conexion));
 
     if ($reg = mysqli_fetch_array($registros)) {
-        mysqli_query($conexion, "DELETE FROM alumnos WHERE mail='$_REQUEST[correo]'") or
+        mysqli_query($conexion, "DELETE FROM alumno WHERE correo='$_REQUEST[correo]'") or
             die("Problemas en el select:" . mysqli_error($conexion));
         echo "Se efectuó el borrado del alumno con dicho mail.";
     } else {
