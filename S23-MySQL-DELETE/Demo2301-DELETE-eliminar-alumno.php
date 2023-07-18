a98ead8ff8ce2d80fbc4e8839d41bbf9dda3d4e2
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-07-18 14:30:42 -0500

    Script para eliminar un alumno a partir de su correo

---
+++ S23-MySQL-DELETE/Demo2301-DELETE-eliminar-alumno.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <?php
+    $conexion = mysqli_connect("localhost", "root", "", "academia") or
+        die("Problemas con la conexión");
+
+    $registros = mysqli_query($conexion, "SELECT codigo FROM alumnos
+                        WHERE mail='$_REQUEST[correo]'") or
+        die("Problemas en el select:" . mysqli_error($conexion));
+
+    if ($reg = mysqli_fetch_array($registros)) {
+        mysqli_query($conexion, "DELETE FROM alumnos WHERE mail='$_REQUEST[correo]'") or
+            die("Problemas en el select:" . mysqli_error($conexion));
+        echo "Se efectuó el borrado del alumno con dicho mail.";
+    } else {
+        echo "No existe un alumno con ese mail.";
+    }
+
+    mysqli_close($conexion);
+    ?>
+</body>
+
+</html>
